600316a78125b9b6a8a79e7666d6dc7c42685f4e
Adam MacKinnon <mackinnon.adam@gmail.com>
2022-01-25 21:29:43 -0500

    added node infastructure

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mustacheExpress = require('mustache-express');
+const path = require('path');
+const PORT = 3000;
+
+
+const VIEWS_PATH = path.join(__dirname,'/views');
+app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials','.mustache'));
+app.set('views', VIEWS_PATH);
+app.set('view engine','mustache');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(express.static('public'));
+
+app.use(require('./routes/index'));
+
+app.get('/', (req,res) => {
+  res.render('index')
+});
+
+app.listen(PORT,() => {
+  console.log(`Server running on ${PORT}`)
+});
