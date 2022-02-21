f24a0c2e338ac466cfb5bd9ee28995a8d857e39d
Adam MacKinnon <mackinnon.adam@gmail.com>
2022-02-20 22:57:56 -0500

    refactored code for emailjs

--- public/js/formLogic.js
+++ public/js/formLogic.js
@@ -9,32 +9,34 @@ contactForm.addEventListener('submit', (e)=>{
     e.preventDefault();
     
     alert("Thank you for your Submission!  We will be in Touch Soon!");
+    window.open('/');
     
-    let formData = {
-        email: email.value,
-        phone: phone.value,
-        service: service.value,
-        city: city.value,
-        state: state.value,
-        comments: comments.value
-    }
-   let xhr = new XMLHttpRequest();
-   xhr.open('POST','/contact');
-   xhr.setRequestHeader('content-type', 'application/json');
-   xhr.onload = function() {
-       console.log(`RESPONSE: ${xhr.responseText}`);
-       if(xhr.responseText == 'success') {
-           alert('email sent');
-           email.value = '';
-           phone.value = ''
-           service.value = '';
-           city.value = '';
-           state.value = '';
-           comments.value = '';
-       } else {
-            console.log('error')
-       }
-   }
+    // let formData = {
+    //     email: email.value,
+    //     phone: phone.value,
+    //     service: service.value,
+    //     city: city.value,
+    //     state: state.value,
+    //     comments: comments.value
+    // }
+//    let xhr = new XMLHttpRequest();
+//    xhr.open('POST','/contact');
+//    xhr.setRequestHeader('content-type', 'application/json');
+//    xhr.onload = function() {
+//        console.log(`RESPONSE: ${xhr.responseText}`);
+//        if(xhr.responseText == 'success') {
+//            alert('email sent');
+//            email.value = '';
+//            phone.value = ''
+//            service.value = '';
+//            city.value = '';
+//            state.value = '';
+//            comments.value = '';
+//        } else {
+//             console.log('error')
+//        }
+//    }
 
-   xhr.send(JSON.stringify(formData));
+
+//    xhr.send(JSON.stringify(formData));
 });
--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const email = require('emailjs');
+const emailjs = require('emailjs');
 const { gmailServiceId, gmailTemplate } = require('../public/js/config');
+const { SMTPClient } = require('emailjs');
 const router = express.Router();
 router.use(express.json());
 
@@ -10,14 +11,36 @@ router.get('/contact', (req,res) => {
 });
 
 router.post('/contact', (req,res) =>{
+
+    let email = req.body.email
+    let phone = req.body.phone
+    let service = req.body.service
+    let city = req.body.city
+    let state = req.body.state
+    let comment = req.body.comment
     console.log(req.body);
 
-    const sendParams = {
-        name: 'Crim Lawn and Landscape',
-        notes: 'Customer Request'
-    }
+const client = new SMTPClient({
+    user: 'crimslawnandlandscape',
+    password: 'Supercrimian#1010',
+    host: 'smtp.gmail.com',
+    ssl: true
+})
 
-    email.send(`${gmailServiceId}, ${gmailTemplate}`,sendParams)
+client.send({
+    from: `${email}`,
+    to: 'crimslawnandlandscape@gmail.com',
+    subject: `${service} in ${city}, ${state}`,
+    text: `
+    Client Phone Number: ${phone}\n
+    Comments: ${comment}
+    `
+},
+(err, message) =>{
+    console.log(err || message);
+})
+
+    emailjs.send(`${gmailServiceId}, ${gmailTemplate}`,sendParams)
     .then(function(res) {
         console.log('SUCCESS!', res.status, res.text);
     }, function(error) {
