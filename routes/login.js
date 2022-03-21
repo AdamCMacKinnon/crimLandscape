fd7510ad328915d7f0321a5566443ac6c8773a80
Adam MacKinnon <mackinnon.adam@gmail.com>
2022-03-20 22:09:28 -0400

    added packages and setup login route

--- routes/login.js
+++ routes/login.js
@@ -7,6 +7,29 @@ router.get('/admin', (req,res) => {
   res.render('admin/login');
 });
 
+router.post('/admin/login', async (req,res) => {
+  let userName = req.body.userName
+  let password = req.body.password
+
+  let admin = await models.Admin.findOne({
+    where: {
+      userName: userName
+    }
+  })
+  if (userName != null) {
+    bcypt.compare(password, admin.password,(error, result) => {
+      if (result) {
+        if (req.session) {
+          req.session.admin = { userName: user }
+          res.redirect('/admin/dashboard');
+        } else {
+          res.render('/admin/login', { message: "Credentials Do Not Match!"})
+        }
+      }
+    })
+  }
+})
+
 
 
 
--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-sequelize model:generate --name contact --attributes email:string,phone:string,service:string,city:string,state:string,comment:string
+# sequelize model:generate --name contact --attributes email:string,phone:string,service:string,city:string,state:string,comment:string
+sequelize model:generate --name admin --attributes userName:string,password:string
